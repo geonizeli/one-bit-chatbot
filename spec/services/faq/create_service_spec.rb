e84414bead2f47b79efa4e81bd2a233c593d0fc4
João <jvsouzax@gmail.com>
2020-01-16 23:58:09 -0300

    Add services

---
+++ spec/services/faq/create_service_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require_relative './../../spec_helper.rb'
+
+describe FaqModule::CreateService do
+  before do
+    @question = FFaker::Lorem.sentence
+    @answer = FFaker::Lorem.sentence
+    @hashtags = "#{FFaker::Lorem.word}, #{FFaker::Lorem.word}"
+  end
+
+  describe '#call' do
+    context 'Without hashtag params' do
+      it 'will receive a error' do
+        @create_service = FaqModule::CreateService.new('question' => @question, 'answer' => @answer)
+        response = @create_service.call
+        expect(response).to match('Hashtag Obrigatória')
+      end
+    end
+
+    context 'With Valid params' do
+      before do
+        @create_service = FaqModule::CreateService.new('question' => @question, 'answer' => @answer, 'hashtags' => @hashtags)
+        @response = @create_service.call
+      end
+
+      it 'Receive success message' do
+        expect(@response).to match('Criado com sucesso')
+      end
+
+      it 'Question and anwser is present in database' do
+        expect(Faq.last.question).to match(@question)
+        expect(Faq.last.answer).to match(@answer)
+      end
+
+      it 'Hashtags are created' do
+        expect(@hashtags.split(/[\s,]+/).first).to match(Hashtag.first.name)
+        expect(@hashtags.split(/[\s,]+/).last).to match(Hashtag.last.name)
+      end
+    end
+  end
+end
